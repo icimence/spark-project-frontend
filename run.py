8f3886475b6f4261161f1ea13852526477d5f89f
icimence <icimence@outlook.com>
2020-11-27 13:08:36 +0800

    add things

--- run.py
+++ run.py
@@ -1,42 +1,20 @@
 from flask import Flask, render_template,jsonify
 from random import *
-from flask_socketio import SocketIO
-from kafka import KafkaConsumer
 import time
-consumer = KafkaConsumer('result',bootstrap_servers=["188.131.179.244:9092"])
-result = ""
 
-def background_thread():
-	global consumer
-	global result
-    for msg in consumer:
-        data_json = msg.value.decode('utf8')
-        data_list = json.loads(data_json)
-        for data in data_list:
-            if '0' in data.keys():
-                girl = data['0']
-            elif '1' in data.keys():
-                boy = data['1']
-            else:
-                continue
-        result = str(girl) + ',' + str(boy)
-        print(result)
 
 app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-thread = None
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
 @app.route('/api/random')
 def random_number():
-	global result
 	response = {
-		'randomNumber' : result
+		'randomNumber' : randint(0,100)
 	}
 	return jsonify(response)
 
@@ -157,4 +135,4 @@ def get_words():
 	return response
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True)
+    app.run()
